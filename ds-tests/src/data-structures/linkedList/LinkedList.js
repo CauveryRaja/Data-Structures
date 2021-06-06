3f2ffc6b60f3ddc2d1ccdfd35cdc7eccecf9164b
CauveryRaja <cauveryraja25@gmail.com>
2021-06-06 10:17:52 +0530

    Implement Insert & Get operations in Linked List

--- ds-tests/src/data-structures/linkedList/LinkedList.js
+++ ds-tests/src/data-structures/linkedList/LinkedList.js
@@ -4,15 +4,34 @@ class LinkedList {
         this.length = 0;
     }
 
-    insert(data, index) {
+    insert(data, index=this.length) {
         let node = new Node(data, null);
         if(!this.root) {
             this.root = node;
         }
         else {
-
+            let currNode, count;
+            currNode = this.root;
+            count = 0;
+            while(count < index-1) {
+                currNode = currNode.next;
+                count++;
+            }
+            node.next = currNode.next;
+            currNode.next = node;
         }
         this.length++;
+    }
+
+    get(index) {
+        let currNode, count;
+        currNode = this.root;
+        count = 0;
+        while(count < index) {
+            currNode = currNode.next;
+            count++;
+        }
+        return currNode.data;
     }
 }
 
--- ds-tests/src/data-structures/linkedList/LinkedList.test.js
+++ ds-tests/src/data-structures/linkedList/LinkedList.test.js
@@ -8,8 +8,16 @@ describe('Linked List', () => {
 
     it('should create Linked List', () => {
         let list = new LinkedList();
+        expect(list).toBeDefined()
+    });
+
+    it('should insert and get 2 items', () => {
+        let list = new LinkedList();
         list.insert(10);
         list.insert(15);
-        expect(list).toBeDefined()
+        expect(list.length).toEqual(2);
+
+        expect(list.get(0)).toEqual(10);
+        expect(list.get(1)).toEqual(15);
     });
 });
