bc08cc0666881dfb77f01e3a4476fe96df7d07f5
CauveryRaja <cauveryraja25@gmail.com>
2022-02-27 13:09:29 +0530

    Implement insertion and deletion operations in Binary Search Tree

--- ds-tests/src/data-structures/binarySearchTree/Feb05_2022/BinarySearchTree.js
+++ ds-tests/src/data-structures/binarySearchTree/Feb05_2022/BinarySearchTree.js
@@ -2,12 +2,57 @@ class BinarySearchTree {
 
     constructor() {
         this.tree = undefined;
-        this.length = 0;
     }
 
     insert(data) {
-        
-        this.length++;
+        let node = new Node(data);
+        if(!this.tree) {
+            this.tree = node;
+        }
+        else {
+           this.insertByTraversing(this.tree, node);
+        }
+    }
+
+    insertByTraversing(parent, node) {
+        let insertAs = node.data < parent.data ? 'leftChild' : 'rightChild';
+        if(!parent[insertAs]) {
+            parent[insertAs] = node;
+        }
+        else {
+            this.insertByTraversing(parent[insertAs], node);
+        }
+    }
+
+    delete(data) {
+        this.deleteByTraversing(this.tree, data);
+    }
+
+    deleteByTraversing(parent, data) {
+        if(parent.leftChild.data === data || parent.rightChild.data === data) {
+            let deleteAt = data === parent.leftChild.data ? 'leftChild' : 'rightChild';
+            // Target node is leaf
+            if(!parent[deleteAt].leftChild && !parent[deleteAt].rightChild) {
+                parent[deleteAt] = undefined;
+            }
+            // Target node has only one child
+            else if(!parent[deleteAt].leftChild || !parent[deleteAt].rightChild) {
+                parent[deleteAt] = parent[deleteAt].leftChild ? parent[deleteAt].leftChild : parent[deleteAt].rightChild;
+            }
+            // Target node has two childs
+            else {
+                let temp = parent[deleteAt].rightChild;
+                parent[deleteAt] = parent[deleteAt].leftChild;
+                if(parent[deleteAt].rightChild) {
+                    this.insertByTraversing(temp, parent[deleteAt].rightChild);
+                }
+                parent[deleteAt].rightChild = temp;
+            }
+        }
+        else {
+            let child = data < parent.data ? 'leftChild' : 'rightChild';
+            this.deleteByTraversing(parent[child], data);
+        }
     }
 };
 
@@ -19,4 +64,17 @@ class Node {
     }
 }
 
-export default BinarySearchTree;
+// export default BinarySearchTree;
+
+let bst = new BinarySearchTree();
+bst.insert(5);
+bst.insert(2);
+bst.insert(8);
+bst.insert(4);
+bst.insert(1);
+bst.insert(0);
+bst.insert(7);
+bst.insert(2);
+
+bst.delete(2);
+console.log(bst);
