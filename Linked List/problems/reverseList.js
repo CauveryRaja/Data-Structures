061d69689e7f8be2c4daa040f1b14bfb32264058
CauveryRaja <cauveryraja25@gmail.com>
2022-08-18 10:41:50 +0530

    Update Linked list reverse logic using recursive approach

--- Linked List/problems/reverseList.js
+++ Linked List/problems/reverseList.js
@@ -15,12 +15,27 @@ let list = {
 function reverse(list) {
     let head = list;
 
-    while(head.next) {
-        let prevHead = head;
-        head = head.next;
-        head.next = prevHead;
-    }
+    console.log('before loop', head);
+    // while(head.next) {
+    //     console.log('in loop', head);
+    //     let prevHead = head;
+    //     head = head.next;
+    //     head.next = prevHead;
+    // }
     return head;
+}
+
+function reverse(list) {
+    recursiveReverse(list);
+}
+
+function recursiveReverse(currNode) {
+    if(currNode.next) {
+        let prevNode = recursiveReverse(currNode.next);
+        prevNode.next = currNode;
+        return prevNode; // Check this
+    }
+    return currNode;
 }
 
 console.log(reverse(list));
