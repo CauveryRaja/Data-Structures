30867937564d02c897cee37628cbf85459e3c807
CauveryRaja <cauveryraja25@gmail.com>
2023-05-21 13:17:53 +0530

    Start tree problem - symmetric

---
+++ Tree/problems/isSymmetric.js
@@ -0,0 +1,10 @@
+// level will act as Queue
+// nextLevel initially like a stack
+
+function isSymmetric(tree) {
+    let level = [], nextLevel = [];
+
+    while(level.length) {
+        let  = 
+    }
+}
